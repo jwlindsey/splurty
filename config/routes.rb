442af0e6dc029b800cde48eea7c3af3f2056c267
jwlindsey <j.w.lindsey1986@gmail.com>
2016-11-04 12:59:21 -0500

    built the about page

--- config/routes.rb
+++ config/routes.rb
@@ -57,4 +57,6 @@ Rails.application.routes.draw do
 
   resources :quotes
   # The priority is based upon order of creation: first created -> highest priority.
+
+  get 'about', to: 'quotes#about'
 end
